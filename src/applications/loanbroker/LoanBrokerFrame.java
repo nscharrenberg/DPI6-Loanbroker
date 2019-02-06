d343c6c884a148edef1c0381b32beda870a8b56e
Noah Scharrenberg <nscharrenberg@hotmail.com>
2019-02-06 19:48:13 +0100

    Broker: JMS logic added

--- src/applications/loanbroker/LoanBrokerFrame.java
+++ src/applications/loanbroker/LoanBrokerFrame.java
@@ -2,13 +2,17 @@ package applications.loanbroker;
 
 import mix.model.bank.BankInterestReply;
 import mix.model.bank.BankInterestRequest;
+import mix.model.loan.LoanReply;
 import mix.model.loan.LoanRequest;
+import org.apache.activemq.ActiveMQConnectionFactory;
 
 import java.awt.EventQueue;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.io.Serializable;
 
+import javax.jms.*;
 import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
 import javax.swing.JList;
@@ -26,6 +30,23 @@ public class LoanBrokerFrame extends JFrame {
 	private JPanel contentPane;
 	private DefaultListModel<JListLine> listModel = new DefaultListModel<JListLine>();
 	private JList<JListLine> list;
+
+	/**
+	 * JMS Variables
+	 */
+	private Connection connection = null;
+	private Session session = null;
+
+	private Destination sendDestination = null;
+
+	private MessageProducer producer = null;
+	private MessageConsumer consumer = null;
+
+	// Queues
+	private Queue loanRequestQueue = null;
+	private Queue loanReplyQueue = null;
+	private Queue bankInterestReplyQueue = null;
+	private Queue bankInterestRequestQueue = null;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -69,6 +90,23 @@ public class LoanBrokerFrame extends JFrame {
 
 		list = new JList<JListLine>(listModel);
 		scrollPane.setViewportView(list);
+
+		/**
+		 * Process
+		 * ======================================================================================
+		 * 1. Client should request a loan, it sends the LoanRequest object to the Loan Broker.
+		 * 2. The Loan Broker consumes the LoanRequest object from the client. 					 <===
+		 * 3. The Loan Broker converts the LoanRequest into a BankInterestRequest.				 <===
+		 * 4. The Loan Broker sends the BankInterestRequest to the Bank.						 <===
+		 * 5. The Bank consumes the BankInterestRequest from the Loan Broker.
+		 * 6. An person sets an interest rate and converts it into a BankInterestReply.
+		 * 7. The Bank sends the BankInterestReply back to the Loan Broker.
+		 * 8. The Loan Broker consumes the BankInterestReply from the Bank.						 <===
+		 * 9. The Loan Broker converts the BankInterestReply into a LoanReply.					 <===
+		 * 10. The Loan Broker sends the LoanReply back to the Client.							 <===
+		 * 11. The Client consumes the LoanReply.
+		 * ======================================================================================
+		 */
 	}
 
 	private JListLine getRequestReply(LoanRequest request){
@@ -101,6 +139,97 @@ public class LoanBrokerFrame extends JFrame {
 		if (rr!= null && bankReply != null){
 			rr.setBankReply(bankReply);;
 			list.repaint();
+		}
+	}
+
+	/**
+	 * This method produces a JMS message and sends it to the Queue.
+	 * @param obj
+	 */
+	private void produce(Serializable obj) {
+		try {
+			if(connection == null) {
+				openJMSConnection();
+			}
+
+			if(obj instanceof LoanRequest) {
+				System.out.println("Producer set");
+				producer = session.createProducer(loanRequestQueue);
+			} else if (obj instanceof LoanReply) {
+				System.out.println("Producer set");
+				producer = session.createProducer(loanReplyQueue);
+			} else if (obj instanceof BankInterestRequest) {
+				producer = session.createProducer(bankInterestRequestQueue);
+			} else if (obj instanceof  BankInterestReply) {
+				producer = session.createProducer(bankInterestReplyQueue);
+			}
+
+			System.out.println("ObjectMessage Created");
+			ObjectMessage msg = session.createObjectMessage(obj);
+			producer.send(msg);
+			System.out.println("ObjectMessage Send");
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * This method consumes a JMS message and handles it's request.
+	 *
+	 * @param queue
+	 * @param listener
+	 */
+	private void consume(String queue, MessageListener listener) {
+		try {
+			if(connection == null) {
+				openJMSConnection();
+			}
+
+			if(queue.equals("loanRequestQueue")) {
+				System.out.println("Consume set");
+				consumer = session.createConsumer(loanRequestQueue);
+			} else if(queue.equals("loanReplyQueue")) {
+				System.out.println("Consume set");
+				consumer = session.createConsumer(loanReplyQueue);
+			} else if (queue.equals("bankInterestRequestQueue")) {
+				System.out.println("Consume set");
+				consumer = session.createConsumer(bankInterestRequestQueue);
+			} else if (queue.equals("bankInterestReplyQueue")) {
+				System.out.println("Consume set");
+				consumer = session.createConsumer(bankInterestReplyQueue);
+			}
+
+			System.out.println("Get ObjectListener");
+			consumer.setMessageListener(listener);
+			connection.start();
+			System.out.println("Start Connection");
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * This method initiates the JMS connection for the Queue's.
+	 * It establishes an ActiveMQ connection and the corresponding Queue's.
+	 */
+	private void openJMSConnection() {
+		try {
+			ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory();
+			connection = factory.createConnection();
+			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+			// Trust all serializable classes
+			factory.setTrustAllPackages(true);
+
+			loanRequestQueue = session.createQueue("loanRequestQueue");
+			loanReplyQueue = session.createQueue("loanReplyQueue");
+			bankInterestRequestQueue = session.createQueue("bankInterestRequestQueue");
+			bankInterestReplyQueue = session.createQueue("bankInterestReplyQueue");
+
+			System.out.println("Connection Opened");
+
+		} catch (JMSException e) {
+			e.printStackTrace();
 		}
 	}
 
