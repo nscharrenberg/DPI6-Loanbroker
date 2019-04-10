b57e8a5dfc9c13d7daa59bd9043d7912e6672a78
Noah Scharrenberg <nscharrenberg@hotmail.com>
2019-04-10 20:02:58 +0200

    WEEEEERRKKKKKK

--- elect/jobseeker2/src/java/com/nscharrenberg/elect/jobseeker/Main.java
+++ elect/jobseeker2/src/java/com/nscharrenberg/elect/jobseeker/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/home.fxml"));
-        primaryStage.setTitle("Elect - Send your resume");
+        primaryStage.setTitle("Elect Client - Send your resume");
         primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.show();
     }
